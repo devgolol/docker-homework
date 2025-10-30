a49b3141881a96d8ac57f6b0e50a6d05e41147c6
devgolol <yuumi7174@gmail.com>
2025-10-30 17:36:03 +0900

    수정

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const db = mysql.createPool({
+const mydb = mysql.createPool({
   host: 'db',
   user: 'root',
   password: '1234',
@@ -15,9 +15,9 @@ const db = mysql.createPool({
 // DB 연결 대기
 async function connectDB() {
   try {
-    await db.query('SELECT 1'); //DB 연결 상태 확인
-    await db.query('CREATE TABLE IF NOT EXISTS counter (value INT)');
-    await db.query('INSERT INTO counter SELECT 0 WHERE NOT EXISTS (SELECT * FROM counter)'); //테이블 아무것도 없을시 0 넣기
+    await mydb.query('SELECT 1'); //DB 연결 상태 확인
+    await mydb.query('CREATE TABLE IF NOT EXISTS counter (value INT)');
+    await mydb.query('INSERT INTO counter SELECT 0 WHERE NOT EXISTS (SELECT * FROM counter)'); //테이블 아무것도 없을시 0 넣기
     console.log('DB 연결 완료');
   } catch (err) {
     console.log('DB 연결 재시도...');
@@ -28,14 +28,14 @@ connectDB();
 
 // 버튼 숫자 상승값 프론트엔드로 json으로 내용전달
 app.post('/click', async (req, res) => {
-  await db.query('UPDATE counter SET value = value + 1');
+  await mydb.query('UPDATE counter SET value = value + 1');
   const [rows] = await db.query('SELECT value FROM counter');
   res.json({ count: rows[0].value });
 });
 
 // 버튼 숫자 감소값 프론트엔드로 json으로 내용전달
 app.post('/decrease', async (req, res) => {
-  await db.query('UPDATE counter SET value = value - 1');
+  await mydb.query('UPDATE counter SET value = value - 1');
   const [rows] = await db.query('SELECT value FROM counter');
   res.json({ count: rows[0].value });
 });
